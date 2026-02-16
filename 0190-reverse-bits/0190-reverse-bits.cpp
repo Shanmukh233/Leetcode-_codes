17fe1c7992f1eaee90bafaf6063f27131703fc61
Shanmukh233 <shanmukhram83@gmail.com>
2026-02-16 09:53:52 +0530

    Time: 0 ms (100%), Space: 8.2 MB (46.07%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int reverseBits(int n) {
+        vector<int> v(32,0);
+        int res=0;
+        for(int i=31;i>=0;i--)
+        {
+            int b=(n>>i)&1;
+            v[i]=b;
+        }
+        for(int i=31;i>=0;i--)
+        {
+            res += v[31-i]*(1<<i);
+        }
+        return res;
+    }
+};
